8210f4a20ad3f005459ba40c141c5c52d418a9d9
Jan Erik Hanssen <jhanssen@gmail.com>
2025-03-18 22:50:07 -0700

    argument parser and libfmt

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
-int main(int, char**)
+#include "Args.h"
+#include <fmt/format.h>
+
+int main(int argc, char** argv, char** envp)
 {
+    const auto args = ArgsParser::parse(
+        argc, argv, envp, "FLIPPY_",
+        [](const char* msg, std::size_t off, char* arg) {
+            fmt::print(stderr, "Flippy: invalid argument, {} at {} in '{}'\n", msg, off, arg);
+        });
     return 0;
 }
