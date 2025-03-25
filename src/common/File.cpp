069bd4d474dc0edc002c230674ba9679955d6cab
Jan Erik Hanssen <jhanssen@gmail.com>
2025-03-24 19:08:16 -0700

    functions for reading/writing host files

---
+++ src/common/File.cpp
@@ -0,0 +1,62 @@
+#include "File.h"
+
+#include <cstdio>
+
+namespace flippy {
+namespace file {
+
+Result<std::vector<uint8_t>> read(const std::filesystem::path& path)
+{
+    FILE* f = fopen(path.c_str(), "rb");
+    if (!f) {
+        return std::unexpected(Error("cannot open file for read: {}", path));
+    }
+
+    // get file size
+    fseek(f, 0, SEEK_END);
+    const long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    uint8_t buffer[65536];
+    std::vector<uint8_t> data;
+    data.reserve(fsize);
+    while (!feof(f)) {
+        const size_t read = fread(buffer, 1, sizeof(buffer), f);
+        if (read == 0) {
+            fclose(f);
+            return std::unexpected(Error("error reading file: {}", path));
+        }
+        data.insert(data.end(), buffer, buffer + read);
+    }
+
+    fclose(f);
+
+    return std::move(data);
+}
+
+Result<void> write(const std::filesystem::path& path, const std::vector<uint8_t>& data)
+{
+    FILE* f = fopen(path.c_str(), "wb");
+    if (!f) {
+        return std::unexpected(Error("cannot open file for write: {}", path));
+    }
+
+    auto rem = data.size();
+    auto off = 0;
+    while (rem > 0) {
+        const std::size_t towrite = std::min<std::size_t>(rem, 65536);
+        const std::size_t written = fwrite(data.data() + off, 1, towrite, f);
+        if (written != towrite) {
+            fclose(f);
+            return std::unexpected(Error("short write: {} ({} < {})", path, written, towrite));
+        }
+        rem -= written;
+        off += written;
+    }
+
+    fclose(f);
+
+    return {};
+}
+
+}} // namespace flippy::file
---
+++ src/common/File.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Result.h"
+#include <filesystem>
+#include <vector>
+#include <cstdint>
+
+namespace flippy {
+namespace file {
+
+Result<std::vector<uint8_t>> read(const std::filesystem::path& path);
+Result<void> write(const std::filesystem::path& path, const std::vector<uint8_t>& data);
+
+}} //namespace flippy::file
